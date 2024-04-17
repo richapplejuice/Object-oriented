763f9e5b373db8c604c9cd77f570563a6170a9b9
richapplejuice <167203932+richapplejuice@users.noreply.github.com>
2024-04-17 13:55:16 +0300

    Task 4 added

--- Exc1.py
+++ Exc1.py
@@ -1,8 +1,10 @@
 import random
 
+'''
 #task1
 print("Hello")
 
+#☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆#
 
 #task2
 def get_user_numbers(): #function asking user to place numbers
@@ -30,6 +32,7 @@ print("User strings:", user_strings)
 random_numbers = [random.randint(1, 100) for _ in range(10)]
 print("Random numbers:", random_numbers)
 
+#☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆#
 
 #task3
 sorted_user_numbers = sorted(user_numbers)
@@ -37,3 +40,35 @@ print("Sorted user numbers: ", sorted_user_numbers)
 
 sorted_user_strings = sorted(user_strings, key=str.lower) #calculates everything in lowercase
 print("Sorted user strings: ", sorted_user_strings)
+
+#☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆#
+
+'''
+
+#task4
+
+def read_integers():
+    integers = []
+    while True:
+        num = int(input("Enter an number (enter 0 to stop): "))
+        if num == 0:
+            break
+        integers.append(num)
+    return integers
+
+# Counts the amount of negative integers
+def count_negative_integers(integers):
+    count = 0
+    for num in integers:
+        if num < 0:
+            count += 1
+    return count
+
+
+def main():
+    numbers = read_integers()
+    negative_count = count_negative_integers(numbers)
+    print("Number of negative integers:", negative_count)
+
+
+main()
