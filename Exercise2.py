ef12eb834125d8c800606722106a3e5776eff3fb
richapplejuice <167203932+richapplejuice@users.noreply.github.com>
2024-04-18 06:17:26 +0000

    TASK 1

---
+++ Exercise2.py
@@ -0,0 +1,36 @@
+#Task1
+
+'''
+a. Pseudocode:
+Descrpition of a computer program that uses simple 
+english statements to outline the basic logic of the
+program to solve a problem or complete a task. even 
+non-programmers can understand. Pseudocode saves time and avoids 
+problems
+    Example:
+    # Step 1: Display("Hello world")
+    print("Hello world")
+
+b. Programming Algorithm:
+Procedure or a formula used for solving a problem. Describes
+logically and steps how to achieve what we want. Good
+algorithm is effective, clear and easily understandable.
+Could be a mathematical pattern, conditional sentences or
+other programming structures.
+
+c. Data attribute:
+Structure that is used to store a data.
+    Example in HTML: 
+    <div data-role="header"> 
+
+d. Method:
+Associated with a class or object. Defines the behavior or actions
+that objects of the class can perform. 
+
+e. Constructor:
+Special type of function to create an object. Prepares the 
+new object for use, often accepting arguments that the constructor
+uses to set required member variables
+
+'''
+
