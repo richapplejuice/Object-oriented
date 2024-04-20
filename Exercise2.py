ab2620f268d2db27f2b08200124eb561a91d66e4
jimmy trinh <jimmy.trinh@edu.turkuamk.fi>
2024-04-20 06:45:04 +0300

    task 5 added

--- Exercise2.py
+++ Exercise2.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
 
 
 #Task 4
-
+'''
 def factorials(n:int) -> dict: 
     factorial_dict = {} #creates empty dictionary to store factorial results.
     
@@ -128,5 +128,29 @@ k = factorials(5)
 print(k[1])
 print(k[3])
 print(k[5]) # 1 * 1 * 2 * 3 * 4 * 5
+'''
 
 
+
+#task5
+
+def smallest_average(person1: dict, person2: dict, person3: dict) -> dict:
+    
+    person1_avg = (person1['result1'] + person1['result2'] + person1['result3']) / 3
+    person2_avg = (person2['result1'] + person2['result2'] + person2['result3']) / 3
+    person3_avg = (person3['result1'] + person3['result2'] + person3['result3']) / 3
+
+    smallest_avg = min(person1_avg, person2_avg, person3_avg)
+
+    if smallest_avg == person1_avg:
+        return person1
+    elif smallest_avg == person2_avg:
+        return person2
+    else:
+        return person3
+    
+person1 = {"name": "Mary", "result1": 2, "result2": 3, "result3": 3}
+person2 = {"name": "Gary", "result1": 5, "result2": 1, "result3": 8}
+person3 = {"name": "Larry", "result1": 3, "result2": 1, "result3": 1}
+
+print(smallest_average(person1, person2, person3))
