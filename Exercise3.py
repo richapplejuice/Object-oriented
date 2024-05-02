0c5ec1857a2a4b9aaeb812b21d27d5c5f4f694ef
jimmy trinh <jimmy.trinh@edu.turkuamk.fi>
2024-05-02 07:48:35 +0300

    task 1 added

---
+++ Exercise3.py
@@ -0,0 +1,27 @@
+#Execise3
+
+#task1
+'''
+a. Encapsulation:
+One of the fundamental concepts in OOP. Hides the details from
+the outside world, providing some kind of abstraction and prevents
+direct access to the state of an object. Helps with controlling &
+modifying through class methods. 
+
+b. Client:
+In software development clients make requests or call methods on 
+objects to perform specific tasks or operations.
+
+c. Data attributes:
+Variables that belong to a class or object and hold data 
+associated with that class or object. It can store various types of
+data such as integers, strings, lists, dictionaries or even objects.
+
+d. Instance:
+Refers to specific realization or occurence of a class.
+when an object is created based on that class, it is called an
+instance of that class. Instance allowmultiple objects with similar 
+characteristics to exist independetly, each maintaining its own state 
+and behaviour.
+'''
+
