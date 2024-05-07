aec1dc20052fca57eb85f46c0f1fa65ab8035c72
jimmy trinh <jimmy.trinh@edu.turkuamk.fi>
2024-05-07 10:33:07 +0300

    task 2 part 1 & 2 added

--- Exercise4.py
+++ Exercise4.py
@@ -1,7 +1,7 @@
 #Exercise 4
 
 #Task 1
-
+"""
 from collections import Counter #Importing the Counter class
 class ListHelper:
     @classmethod
@@ -19,3 +19,55 @@ class ListHelper:
 numbers = [1, 1, 2, 1, 3, 3, 4, 5, 5, 5, 6, 5, 5, 5]
 print(ListHelper.greatest_frequency(numbers))
 print(ListHelper.doubles(numbers))
+"""
+
+#Task 2
+#Part-1
+class Item:
+    def __init__(self, name, weight):
+        self.__name = name
+        self.__weight = weight
+
+    def name(self):
+        return self.__name
+    
+    def weight(self):
+        return self.__weight
+    
+    def __str__(self):
+        return f"{self.__name} ({self.__weight} g)"
+    
+#Part-2 
+class Suitcase:
+    def __init__(self, max_weight):
+        self.__max_weight = max_weight
+        self.__items = [] 
+        self.__total_weight = 0
+    
+    def add_item(self, item):
+        if self.__total_weight + item.weight() <= self.__max_weight:
+            self.__items.append(item)
+            self.__total_weight += item.weight()
+    
+    #Part-3
+    def __str__(self):
+        num_items = len(self.__items)
+        if num_items == 0:
+            return "0 items (0 g)"
+        elif num_items == 1:
+            return f"1 item ({self.__total_weight} g)"
+        else:
+            return f"{num_items} items ({self.__total_weight} g)"
+    
+    
+book = Item("ABC Book", 200)
+phone = Item("Nokia 3210", 100)
+brick = Item("Brick", 400)
+suitcase = Suitcase(500)
+print(suitcase)
+suitcase.add_item(book)
+print(suitcase)
+suitcase.add_item(phone)
+print(suitcase)
+suitcase.add_item(brick)
+print(suitcase)
