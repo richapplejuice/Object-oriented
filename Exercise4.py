3a53cfe160631952185733690362241ec4b31f10
jimmy trinh <jimmy.trinh@edu.turkuamk.fi>
2024-05-14 09:09:09 +0300

    Task 7 part-1 added

--- Exercise4.py
+++ Exercise4.py
@@ -198,6 +198,7 @@ diminuendo.print_recipe("pocushocus")
 
 #Task 6
 #part-1
+'''
 import random
 
 class Dice:
@@ -239,7 +240,7 @@ if __name__ == "__main__":
     main()
 
 
-'''
+
 dice1 = Dice()
 dice2 = Dice()
 dice3 = Dice()
@@ -248,3 +249,38 @@ dice_list = [dice1, dice2, dice3]
 print("Let's play dice!")
 play_game(dice_list)
 '''
+
+
+#Task 7
+#part-1
+
+class Person:
+    def __init__(self, name):
+        self._name = name
+        self._numbers = []
+        self._address = None
+
+    def name(self):
+        return self._name
+    
+    def numbers(self):
+        return self._numbers
+    
+    def address(self):
+        return self._address 
+    
+    def add_number(self,number):
+        self._numbers.append(number)
+
+    def add_address(self, address):
+        self._address = address
+
+
+person = Person("Eric")
+print(person.name())
+print(person.numbers())
+print(person.address())
+person.add_number("040-123456")
+person.add_address("Mannerheimintie 10 Helsinki")
+print(person.numbers())
+print(person.address())
